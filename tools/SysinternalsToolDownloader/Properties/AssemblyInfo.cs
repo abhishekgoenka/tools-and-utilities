9589bb5504b1c1f34b4e07dd2bb807b5eaa8f21b
Abhishek Goenka <govindkaran@gmail.com>
2016-12-26 11:00:28 +0530

    Completed all bindings

--- tools/SysinternalsToolDownloader/Properties/AssemblyInfo.cs
+++ tools/SysinternalsToolDownloader/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SysinternalsToolDownloader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sysinternals Tools Downloader")]
+[assembly: AssemblyDescription("This utility downloads the all the tools from Sysinternals website and unzip in selected directory. This utility will also add the selected folder path to system variable, so that Sysinternals tools can be run from anywhere")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SysinternalsToolDownloader")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
--- tools/SysinternalsToolDownloader/ViewModel/Downloader.cs
+++ tools/SysinternalsToolDownloader/ViewModel/Downloader.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
 using System.Windows.Input;
 using SysinternalsToolsDownloader.Utilities;
 
@@ -10,11 +14,16 @@ namespace SysinternalsToolsDownloader.ViewModel
     /// </summary>
     public class Downloader : INotifyPropertyChanged
     {
+        private const string DOWNLOAD_URL = "https://download.sysinternals.com/files/SysinternalsSuite.zip";
+        private readonly string TEMP_FILE_PATH = Path.GetTempPath() + "SysinternalsSuite.zip";
         private string _diagMessage;
+        private bool _isDownloadButtonEnabled;
+        private int _lastProgressPercentage;
 
         public Downloader()
         {
             IsExtractToCurrentDirectorySelected = true;
+            IsDownloadButtonEnabled = true;
         }
 
         /// <summary>
@@ -22,9 +31,28 @@ namespace SysinternalsToolsDownloader.ViewModel
         /// </summary>
         public bool IsAddToSystemPathSelected { get; set; }
 
+        /// <summary>
+        ///     TRUE if "Current Directory" is selected
+        /// </summary>
         public bool IsExtractToCurrentDirectorySelected { get; set; }
 
+        /// <summary>
+        ///     TRUE if "C:\Program Files" is selected
+        /// </summary>
         public bool IsExtractToProgramFilesSelected { get; set; }
+
+        /// <summary>
+        ///     Enable/Disable Download button
+        /// </summary>
+        public bool IsDownloadButtonEnabled
+        {
+            get { return _isDownloadButtonEnabled; }
+            set
+            {
+                _isDownloadButtonEnabled = value;
+                RaisePropertyChanged("IsDownloadButtonEnabled");
+            }
+        }
 
         /// <summary>
         ///     Diagnosis logs
@@ -49,12 +77,75 @@ namespace SysinternalsToolsDownloader.ViewModel
         private void OnStartDownloadCommand()
         {
             Log("Downloading started...");
+            IsDownloadButtonEnabled = false;
+
+            using (var wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                wc.DownloadFileAsync(new Uri(DOWNLOAD_URL),
+                    TEMP_FILE_PATH);
+            }
         }
 
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Log($"File downloaded successfully at {TEMP_FILE_PATH}");
+            IsDownloadButtonEnabled = true;
+
+            if (IsExtractToCurrentDirectorySelected)
+                Decompress(TEMP_FILE_PATH, Directory.GetCurrentDirectory());
+            else if (IsExtractToProgramFilesSelected)
+                Decompress(TEMP_FILE_PATH, Directory.GetCurrentDirectory());
+        }
+
+        private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (_lastProgressPercentage != e.ProgressPercentage)
+            {
+                Log($"{e.ProgressPercentage} percentage completed…");
+                _lastProgressPercentage = e.ProgressPercentage;
+            }
+        }
+
+        public void Decompress(string sourceArchiveFileName, string destinationDirectoryName)
+        {
+            destinationDirectoryName += "\\Sysinternals";
+
+            if (Directory.Exists(destinationDirectoryName))
+            {
+                Log($"{destinationDirectoryName} directory already exists!!!");
+                return;
+            }
+            ZipFile.ExtractToDirectory(sourceArchiveFileName, destinationDirectoryName);
+            Log($"Utilities extracted to {destinationDirectoryName}");
+
+
+            if (IsAddToSystemPathSelected)
+                SetEnvironmentVariablePath(destinationDirectoryName);
+        }
+
+        private void SetEnvironmentVariablePath(string directoryPath)
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            if (environmentVariable != null)
+            {
+                var path = environmentVariable.Split(';');
+                if (path.Any(e => string.Compare(e, directoryPath, StringComparison.InvariantCultureIgnoreCase) == 0))
+                {
+                    Log("Directory path already exists in system path");
+                    return;
+                }
+
+                environmentVariable += ";" + directoryPath;
+                Environment.SetEnvironmentVariable("Path", environmentVariable, EnvironmentVariableTarget.Machine);
+            }
+        }
 
         private void Log(string message)
         {
-            DiagMessage += Environment.NewLine + message;
+            if (string.IsNullOrEmpty(DiagMessage)) DiagMessage = message;
+            else DiagMessage += Environment.NewLine + message;
         }
 
         #endregion
